5c78f5763c2e319c77081fda11fb04199a8df671
Cheenar Gupte <wafflecaker@gmail.com>
2016-12-23 04:29:08 -0500

    Version 0.5b

--- Server/myapp/routes/account.js
+++ Server/myapp/routes/account.js
@@ -5,20 +5,46 @@ router.get("/", function (req, res, next) {
   if (req.cookies.name != null && req.cookies.uid != null) {
     var data = null;
     global.connection.query("SELECT * FROM `Users` WHERE `UID` LIKE '" + req.cookies.uid + "'", function (err, rows, fields) {
-      data = rows[0];
-      res.render("account", {
-        'accountLoggedIn': "true",
-        'name': req.cookies.name,
-        "fullName": data.Name,
-        "phoneNumber": data.PhoneNumber,
-        "shippingAddress": data.ShippingAddress,
-        "billingAddress": data.BillingAddress,
-        "emailAddress": data.Email
-      });
+      if(err) {
+        console.log("Error");
+        res.send("account", { 'accountLoggedIn': false });
+      } else {
+        data = rows[0];
+        if(data != null) {
+
+        console.log("Data good");
+        let uid = req.cookies.uid;
+
+        var collection = global.orderDatabase.db.getCollection(uid);
+        var currentData = collection.get(1).orders;
+
+        let uoids = [];
+
+        if(currentData != null) {
+          for(ki in currentData) {
+            if(ki != null) {
+              console.log("UOID:" + currentData[ki].uoid);
+              uoids.push(currentData[ki]);
+            }
+          }
+        }
+
+          res.render("account", {
+            'accountLoggedIn': true,
+            'name': req.cookies.name,
+            "fullName": data.Name,
+            "phoneNumber": data.PhoneNumber,
+            "shippingAddress": data.ShippingAddress,
+            "billingAddress": data.BillingAddress,
+            "emailAddress": data.Email,
+            "uoids": uoids,
+          });
+        } else {
+          console.log("Data null");
+          res.render("account", { 'accountLoggedIn': false});
+        }
+      }
     });
-  }
-  else {
-    res.render("account", { 'accountLoggedIn': "false" });
   }
 });
 
--- Server/myapp/routes/index.js
+++ Server/myapp/routes/index.js
@@ -7,8 +7,12 @@ router.get('/', function (req, res, next) {
   if (name != null && name != "") {
     res.render('index', { welcomeTag: "Welcome, " + name + "." });
   } else {
-    res.render('index', { welcomeTag: "Welcome." }); 
+    res.render('index', { welcomeTag: "" }); 
   }
+});
+
+router.get("/order", function(req, res) {
+  res.render("order", {});
 });
 
 router.get("/logout", function(req, res) {
